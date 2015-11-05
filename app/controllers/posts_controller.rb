a044b475f3ec1c6720d813f5443ffc3559eb0ba2
Cheri <avyishi@gmail.com>
2015-11-04 20:30:55 -0800

    Added post views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post saved!"
+      redirect_to @post
+    else
+      flash[:error] = "There was an issue saving your post, please try again!"
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+   def update
+     @post = Post.find(params[:id])
+     if @post.update_attributes(params.require(:post).permit(:title, :body))
+       flash[:notice] = "Post updated!"
+       redirect_to @post
+     else
+       flash[:error] = "There was an issue saving your post, please try again!"
+       render :edit
+     end
+   end
+
+end
